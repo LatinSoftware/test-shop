b8e78c854769042fdd41736d3221759044c37e0f
Caonabo Mena <cmena7512@gmail.com>
2025-05-13 15:58:43 -0400

    add cart page

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,12 +1,80 @@
-function CartPage() {
-    return (  
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Image from "next/image";
+import Link from "next/link";
 
-        <div>
-            <h1>Cart page</h1>
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+function CartPage() {
+  return (
+    <section className="flex justify-center items-center mb-72 px-10 sm:p-0">
+      <div className="flex flex-col w-[1000px] ">
+        <Title title="Cart" />
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
+          {/** shopping cart */}
+          <div className="flex flex-col mt-5">
+            <span>Add Items</span>
+            <Link href="/home" className="underline mb-5">
+              Continue buying
+            </Link>
+
+            {productsInCart.map((product) => (
+              <div key={product.slug} className="flex mb-5">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  width={100}
+                  height={100}
+                  className="object-cover mr-5 rounded"
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p>${product.price}</p>
+                  <QuantitySelector quantity={3} />
+                  <button className="underline">Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          {/** Order summary */}
+
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">Order summary</h2>
+            <div className="grid grid-cols-2">
+              <span>Products</span>
+              <span className="text-right">3 items</span>
+
+              <span>SubTotal</span>
+              <span className="text-right">$ 100.00</span>
+
+              <span>Tax</span>
+              <span className="text-right">15%</span>
+
+              <span className="mt-5 text-2xl">Total:</span>
+              <span className="mt-5 text-right text-2xl">$ 100.00</span>
+            </div>
+            <div className="mt-5 mb-2 w-full">
+              <Link
+                href="/checkout/address"
+                className="flex btn-primary justify-center"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default CartPage;
-
-
